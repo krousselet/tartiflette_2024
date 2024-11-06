9f757a7be4e6a172fb60576e5b3f2912286c9550
krousselet <krous74@gmail.com>
2024-11-06 10:06:25 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+window.onload = function () {
+    const swiper = new Swiper('.swiper', {
+        direction: 'horizontal',
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        scrollbar: {
+            el: '.swiper-scrollbar',
+        },
+    });
+};
